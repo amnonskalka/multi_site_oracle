a280fc87f609149ff64873448163eb4c964456db
amnonskalka <35772616+amnonskalka@users.noreply.github.com>
2018-08-06 16:31:14 +0300

    Add files via upload
    Error handling
    Change file method handling 
    Add Logging

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import utilities as ut
 import pandas as pd
 
+ut.logging.info('Process started')
 pd.set_option('display.max_columns', 20)
-param_path = r'd:\\sites_json.json'
+param_path = r'd:\sites_json.json'
 parameters_json = ut.parameter_file_load(param_path)
-sql_path = r'd:\\db_query.sql'
+sql_path = r'd:\db_query.sql'
 results = pd.DataFrame(columns=ut.result_columns)
 
 
@@ -15,10 +16,19 @@ for index, row in parameters_json.iterrows():
     db_pass = row['password']
     db_sid = row['sid']
 
-    conn = ut.oracle_connect(db_ip, db_user, db_pass, db_sid)
-    sql = ut.read_query(sql_path)
-    site_results = ut.collect_data(sql, conn, site)
-    results = results.append(site_results)
+    ut.logging.info('Start Working on ' + site.upper())
+
+    try:
+        conn = ut.oracle_connect(db_ip, db_user, db_pass, db_sid)
+        sql = ut.read_query(sql_path)
+        site_results = ut.collect_data(sql, conn, site)
+        results = results.append(site_results)
+        ut.logging.info('Finish working on ' + site.upper())
+    except Exception as e:
+        ut.logging.error(str(e) + 'was raise due to the running of the main script')
+        continue
 
 conn.close()
+
+ut.logging.info('Process finished')
 print(results)
